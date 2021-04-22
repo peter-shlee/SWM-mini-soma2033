207c439661cd0bcdb8eb3124cb058a21edf1aad0
이승현 <shlee4290@naver.com>
2021-04-22 10:27:56 +0000

    update routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const router = express.Router();
 
 const libKakaoWork = require('../libs/kakaoWork');
 
+
 router.get('/', async (req, res, next) => {
   // 유저 목록 검색 (1)
   const users = await libKakaoWork.getUserList();
@@ -18,30 +19,89 @@ router.get('/', async (req, res, next) => {
     conversations.map((conversation) =>
       libKakaoWork.sendMessage({
         conversationId: conversation.id,
-        text: '설문조사 이벤트',
-        blocks: [
-          {
-            type: 'header',
-            text: '☕ 사내 카페 만족도 조사 🥤',
-            style: 'blue',
-          },
-          {
-            type: 'text',
-            text:
-              '어느덧 사내카페가 바뀐지 한달이 되었습니다.\n구르미들이 카페를 이용하고 계신지 의견을 들어보고자 설문 조사를 진행해봅니다!!\n설문에 참여하면 푸짐한 경품 찬스가있으니 상품 꼭 받아가세요! 🎁',
-            markdown: true,
-          },
-          {
-            type: 'button',
-            action_type: 'call_modal',
-            value: 'cafe_survey',
-            text: '설문 참여하기',
-            style: 'default',
-          },
-        ],
+        "text": "SOMA 2033 test",
+  "blocks": [
+    {
+      "type": "header",
+      "text": "SOMA 2033",
+      "style": "blue"
+    },
+    {
+      "type": "image_link",
+      "url": "https://t1.kakaocdn.net/kakaowork/resources/block-kit/imagelink/image3@3x.jpg"
+    },
+    {
+      "type": "text",
+      "text": "당신은 길을 가다가 춥고 험난한 설산을 발견합니다. 시리에 의하면 이이 설산 깊숙히에는 고대 기계룡이 잠자고 있다고 합니다ㅠㅠ",
+      "markdown": true
+    },
+    {
+      "type": "action",
+      "elements": [
+        {
+          "type": "button",
+          "text": "산 안쪽으로",
+          "style": "primary"
+        },
+        {
+          "type": "button",
+          "text": "지나간다",
+          "style": "primary"
+        }
+      ]
+    },
+    {
+      "type": "divider"
+    },
+    {
+      "type": "button",
+      "text": "내 상태 및 소지품",
+      "style": "default"
+    }
+  ],
+
       })
     ),
   ]);
+
+// router.get('/', async (req, res, next) => {
+//   // 유저 목록 검색 (1)
+//   const users = await libKakaoWork.getUserList();
+
+//   // 검색된 모든 유저에게 각각 채팅방 생성 (2)
+//   const conversations = await Promise.all(
+//     users.map((user) => libKakaoWork.openConversations({ userId: user.id }))
+//   );
+
+//   // 생성된 채팅방에 메세지 전송 (3)
+//   const messages = await Promise.all([
+//     conversations.map((conversation) =>
+//       libKakaoWork.sendMessage({
+//         conversationId: conversation.id,
+//         text: '설문조사 이벤트',
+//         blocks: [
+//           {
+//             type: 'header',
+//             text: '☕ 사내 카페 만족도 조사 🥤',
+//             style: 'blue',
+//           },
+//           {
+//             type: 'text',
+//             text:
+//               '어느덧 사내카페가 바뀐지 한달이 되었습니다.\n구르미들이 카페를 이용하고 계신지 의견을 들어보고자 설문 조사를 진행해봅니다!!\n설문에 참여하면 푸짐한 경품 찬스가있으니 상품 꼭 받아가세요! 🎁',
+//             markdown: true,
+//           },
+//           {
+//             type: 'button',
+//             action_type: 'call_modal',
+//             value: 'cafe_survey',
+//             text: '설문 참여하기',
+//             style: 'default',
+//           },
+//         ],
+//       })
+//     ),
+//   ]);
 
   // 응답값은 자유롭게 작성하셔도 됩니다.
   res.json({
