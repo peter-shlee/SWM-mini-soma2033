8a09c064b6241a0e13d9e7707c4d987bd4d75dfb
이승현 <shlee4290@naver.com>
2021-04-24 05:32:17 +0000

    resource 파일의 I/O에 사용할 함수들 작성

---
+++ libs/resIO/index.js
@@ -0,0 +1,39 @@
+const fs = require("fs")
+
+// load json file synchronously
+exports.readJsonSync = (filePath) => {
+	try {
+		const jsonString = fs.readFileSync(filePath)
+		const jsonObject = JSON.parse(jsonString)
+		return jsonObject;
+	} catch (err) {
+		console.log(err)
+		return
+	}
+};
+
+// save json file synchronously
+exports.saveJsonSync = (filePath, jsonObject) => {
+	try {
+		const jsonString = JSON.stringify(jsonObject)
+		fs.writeFileSync(filePath, jsonString)
+		return
+	} catch (err) {
+		console.log(err)
+		return
+	}
+}
+
+// save json file asynchronously
+exports.saveJsonAsync = (filePath, jsonObject, callback) => {
+	const jsonString = JSON.stringify(jsonObject)
+	fs.writeFile(filePath, jsonString, function (err) {
+		if (err) {
+			console.log(err)
+			return
+		} else {
+			callback()
+		}
+	})
+	return
+}
