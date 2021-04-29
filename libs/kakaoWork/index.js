fa600cc4babcbb78a56da2cf3a8146341e09ad92
이승현 <shlee4290@naver.com>
2021-04-29 15:08:21 +0000

    스페이스 내 모든 유저 (100명 이상) 불러오도록 수정

--- libs/kakaoWork/index.js
+++ libs/kakaoWork/index.js
@@ -3,34 +3,45 @@ const Config = require('config');
 
 const axios = require('axios');
 const kakaoInstance = axios.create({
-  baseURL: 'https://api.kakaowork.com',
-  headers: {
-    Authorization: `Bearer ${Config.keys.kakaoWork.bot}`,
-  },
+	baseURL: 'https://api.kakaowork.com',
+	headers: {
+		Authorization: `Bearer ${Config.keys.kakaoWork.bot}`,
+	},
 });
 
 // 유저 목록 검색 (1)
 exports.getUserList = async () => {
-  const res = await kakaoInstance.get('/v1/users.list');
-  return res.data.users;
+	var all_users = [];
+	var res = await kakaoInstance.get('/v1/users.list?limit=50');
+	all_users = all_users.concat(res.data.users)
+	while (res.data.cursor) {
+		try{
+			res = await kakaoInstance.get(`/v1/users.list?cursor=${res.data.cursor}`);
+			all_users = all_users.concat(res.data.users);
+		} catch (err) {
+			console.log(err);
+			break;
+		}
+	}
+	return all_users;
 };
 
 // 채팅방 생성 (2)
 exports.openConversations = async ({ userId }) => {
-  const data = {
-    user_id: userId,
-  };
-  const res = await kakaoInstance.post('/v1/conversations.open', data);
-  return res.data.conversation;
+	const data = {
+		user_id: userId,
+	};
+	const res = await kakaoInstance.post('/v1/conversations.open', data);
+	return res.data.conversation;
 };
 
 // 메시지 전송 (3)
 exports.sendMessage = async ({ conversationId, text, blocks }) => {
-  const data = {
-    conversation_id: conversationId,
-    text,
-    ...blocks && { blocks },
-  };
-  const res = await kakaoInstance.post('/v1/messages.send', data);
-  return res.data.message;
+	const data = {
+		conversation_id: conversationId,
+		text,
+		...(blocks && { blocks }),
+	};
+	const res = await kakaoInstance.post('/v1/messages.send', data);
+	return res.data.message;
 };
